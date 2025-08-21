80ff82f05dbca814fd190c2835367e68e46e203c
Elisabeth Solmunde <elisabeth.solmunde@gmail.com>
2025-08-21 15:59:11 +0200

    added file wrangling added file wrangling_proces_presentation added file plots

--- wrangling.R
+++ wrangling.R
@@ -43,6 +43,27 @@ income_groups <- life_expectancy |>
   filter(str_detect(Entity, regex("income", ignore_case = TRUE))) |>
   select(-Code)
 
+# Adding continent, region and decade to life_expectancy_female_male
+
+life_expectancy_female_male_regions <- life_expectancy_female_male |>
+  mutate(Decade = case_when(Year >= 1950 & Year < 1960 ~ 1950,
+                            Year >= 1960 & Year < 1970 ~ 1960,
+                            Year >= 1970 & Year < 1980 ~ 1970,
+                            Year >= 1980 & Year < 1990 ~ 1980,
+                            Year >= 1990 & Year < 2000 ~ 1990,
+                            Year >= 2000 & Year < 2010 ~ 2000,
+                            Year >= 2010 & Year < 2020 ~ 2010)) |>
+  filter(!is.na(Decade)) |>
+  filter(!Entity %in% c("Africa", "Asia", "Europe", "Northern America", "Americas", "Oceania")) |>
+  mutate(
+    Continent = countrycode(sourcevar = Entity,
+                            origin = "country.name",
+                            destination = "continent"),
+    Region = countrycode(sourcevar = Entity,
+                         origin = "country.name",
+                         destination = "region")
+  ) |>
+  filter(!is.na(Region))
 
 
 
